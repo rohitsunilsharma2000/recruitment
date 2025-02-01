794a30f673573f9901f6a074a5e7912f26d26ef0
debnath <dsdebnath4663@gmail.com>
2025-02-01 22:33:47 +0530

    JobAccordion & AllJobOpening tabil data population added

--- app/job-openings/description/JobAccordion.js
+++ app/job-openings/description/JobAccordion.js
@@ -14,18 +14,18 @@ function JobAccordion() {
 
 
 
-    useEffect(() => {
-      async function fetchJobOpeningByIdData() {
-        try {
-          const response = await fetchJobOpeningById(id);
-          console.log("Fetched all Job Opening :", response); // Log the response for debugging
-          setJobOpeningData(response); // Store the response data in state
-        } catch (error) {
-          setJobOpeningData([]); // Set an empty array in case of error
-        }
+  useEffect(() => {
+    async function fetchJobOpeningByIdData() {
+      try {
+        const response = await fetchJobOpeningById(id);
+        console.log("Fetched all Job Opening :", response); // Log the response for debugging
+        setJobOpeningData(response); // Store the response data in state
+      } catch (error) {
+        setJobOpeningData([]); // Set an empty array in case of error
       }
-      fetchJobOpeningByIdData(); // Trigger the API call when the component mounts
-    }, []);
+    }
+    fetchJobOpeningByIdData(); // Trigger the API call when the component mounts
+  }, []);
 
   // useEffect(() => {
   //   // Fetch data from API
@@ -47,133 +47,134 @@ function JobAccordion() {
   if (!jobOpeningData) return <p>Loading....</p>;
 
 
-    return (
-      <div className="accordion" id="jobOpeningAccordion">
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingJobOpening">
-            <button
-              className="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseJobOpening"
-              aria-expanded="true"
-              aria-controls="collapseJobOpening"
-            >
-              Job Opening Information
-            </button>
-          </h2>
-          <div
-            id="collapseJobOpening"
-            className="accordion-collapse collapse show"
-            aria-labelledby="headingJobOpening"
-            data-bs-parent="#jobOpeningAccordion"
+  return (
+    <div className="accordion" id="jobOpeningAccordion">
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingJobOpening">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseJobOpening"
+            aria-expanded="true"
+            aria-controls="collapseJobOpening"
           >
-                   
+            Job Opening Information
+          </button>
+        </h2>
+        <div
+          id="collapseJobOpening"
+          className="accordion-collapse collapse show"
+          aria-labelledby="headingJobOpening"
+          data-bs-parent="#jobOpeningAccordion"
+        >
 
-            <div className="accordion-body">
 
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Posting Title:</strong> {jobOpeningData.postingTitle}
-                </div>
-                <div className="col-md-6">
-                  <strong>Assigned Recruiter(s):</strong>{" "}
-                  saha@bishnupadasaha.agency
-                </div>
+          <div className="accordion-body">
+
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <strong>Posting Title:</strong> {jobOpeningData.postingTitle}
               </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Target Date:</strong> 01/18/2025
-                </div>
-                <div className="col-md-6">
-                  <strong>Job Opening Status:</strong> Waiting for approval
-                </div>
+              <div className="col-md-6">
+                <strong>Assigned Recruiter(s):</strong>{" "}
+                saha@bishnupadasaha.agency
               </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Industry:</strong> Accounting
-                </div>
-                <div className="col-md-6">
-                  <strong>Salary:</strong> 
-                </div>
-              </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Created By:</strong> saha@bishnupadasaha.agency
-                </div>
-                <div className="col-md-6">
-                  <strong>Created On:</strong> Thu, 19 Dec 2024 05:30 AM
-                </div>
-              </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Job Opening ID:</strong> ZR_3_JOB
-                </div>
-                <div className="col-md-6">
-                  <strong>Department Name:</strong> Paula Rojas (Sample)
-                </div>
-              </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Hiring Manager:</strong> saha@bishnupadasaha.agency
-                </div>
-                <div className="col-md-6">
-                  <strong>Number of Positions:</strong> 5
-                </div>
-              </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Date Opened:</strong> 12/19/2024
-                </div>
-                <div className="col-md-6">
-                  <strong>Job Type:</strong> Full time
-                </div>
-              </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Work Experience:</strong> Fresher
-                </div>
-                <div className="col-md-6">
-                  <strong>Required Skills:</strong> 
-                </div>
-              </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Modified By:</strong> saha@bishnupadasaha.agency
-                </div>
-                <div className="col-md-6">
-                  <strong>Modified On:</strong> Thu, 30 Jan 2025 11:15 AM
-                </div>
-              </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>City:</strong> Tallahassee
-                </div>
-                <div className="col-md-6">
-                  <strong>State/Province:</strong> Florida
-                </div>
-              </div>
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <strong>Country:</strong> United States
-                </div>
-                <div className="col-md-6">
-                  <strong>Zip/Postal Code:</strong> 32301
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <strong>Job Description:</strong>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-              </div>
-            
             </div>
-        
-            {/* accordion-body */}
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <strong>Target Date:</strong> {jobOpeningData.targetDate}
+              </div>
+              <div className="col-md-6">
+                <strong>Job Opening Status:</strong>  {jobOpeningData.targetDate}
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <strong>Industry:</strong>  {jobOpeningData.industry}
+              </div>
+              <div className="col-md-6">
+                <strong>Salary:</strong> {jobOpeningData.salary}
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <strong>Created By:</strong>  {jobOpeningData.createdBy}
+              </div>
+              <div className="col-md-6">
+                <strong>Created On:</strong>   {jobOpeningData.createdOn}
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <strong>Job Opening ID:</strong> ZR_3_JOB
+              </div>
+              <div className="col-md-6">
+                <strong>Department Name:</strong>  {jobOpeningData.departmentName}
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <strong>Hiring Manager:</strong> saha@bishnupadasaha.agency  {jobOpeningData.hiringManager}
+              </div>
+              <div className="col-md-6">
+                <strong>Number of Positions:</strong> 5 {jobOpeningData.numberOfPositions}
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <strong>Date Opened:</strong> 12/19/2024 {jobOpeningData.dateOpened}
+              </div>
+            </div>
+            <div className="col-md-6">
+              <strong>Job Type:</strong> Full time  {jobOpeningData.jobType}
+            </div>
           </div>
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <strong>Work Experience:</strong> Fresher   {jobOpeningData.workExperience}
+            </div>
+            <div className="col-md-6">
+              <strong>Required Skills:</strong>  {jobOpeningData.requiredSkills}
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <strong>Modified By:</strong> saha@bishnupadasaha.agency  {jobOpeningData.modifiedBy}
+            </div>
+            <div className="col-md-6">
+              <strong>Modified On:</strong> Thu, 30 Jan 2025 11:15 AM  {jobOpeningData.modifiedOn}
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <strong>City:</strong> Tallahassee {jobOpeningData.city}
+            </div>
+            <div className="col-md-6">
+              <strong>State/Province:</strong> Florida {jobOpeningData.stateProvince}
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <strong>Country:</strong> United States  {jobOpeningData.country}
+            </div>
+            <div className="col-md-6">
+              <strong>Zip/Postal Code:</strong> 32301 {jobOpeningData.zipPostalCode}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <strong>Job Description:</strong> {jobOpeningData.jobDescription}
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+
         </div>
+
+        {/* accordion-body */}
+
       </div>
-    );
-  }
-  export default JobAccordion;
+    </div>
+  );
+}
+export default JobAccordion;
